b9433e32df3e7da7f1d9ddab8b7eca0b880b8b94
icocowen <1558165507@qq.com>
2019-12-16 08:43:33 +0800

    初始化项目

---
+++ src/main/java/com/iwne/dorm/info/mangement/DormInfoMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iwne.dorm.info.mangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormInfoMangementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormInfoMangementSystemApplication.class, args);
+    }
+
+}
